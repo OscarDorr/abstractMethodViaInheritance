6fd8f5eb1c414b3c7cd59cbff152da949c11b542
Adam Cohen <47546485+AdamCohen0@users.noreply.github.com>
2019-03-03 22:14:21 -0500

    an epic addition

--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -3,4 +3,8 @@ public class UserOfSpeakers{
     Animal fido = new Retriever();
     System.out.println("The dog says " + fido.speak());
   }
+  
+  public int add(int a, int b) { //this method is completely, and utterly, useless.
+      return a + b;
+  }
 }
